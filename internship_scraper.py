f615b6bdc271e5cde3a03afe438464a62e1a21fe
Jithendra <jith05122005@gmail.com>
2025-07-15 13:46:31 +0530

    Fix: added root route and changed host/port for Render

--- internship_scraper.py
+++ internship_scraper.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
@@ -10,6 +11,10 @@ from selenium.webdriver.common.by import By
 import time
 
 app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Internship Scraper API is running. Visit /internships to get data."
+
 
 # ---------- Internshala ----------
 def scrape_internshala():
@@ -149,4 +154,5 @@ def internships():
 
 # ---------- Main ----------
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
